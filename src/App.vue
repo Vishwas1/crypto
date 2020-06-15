11694a371666e15509f22b31c7d55ef88d41274d
vishwas1 <vishwasanand.cse@gmail.com>
2020-06-16 00:54:05 +0530

    fixed minor bugs

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <style scoped>
-.logo{
-	/* width: 144px; */
+.logo {
+  /* width: 144px; */
   padding-top: 1.5%;
   padding-left: 25px;
-  
 }
-.leftAlign{
+.leftAlign {
   text-align: left;
 }
 </style>
 <template>
   <div id="app">
-    <div class="row ">
+    <div class="row">
       <div class="col-md-8 centeralign logo leftAlign">
         <!-- <h3><img class="logo" src="https://forum.aeternity.com/uploads/db0917/original/1X/543f494a818af66690a1298689af3fdd0c6389a1.png"> | Laboratory</h3> -->
         <h2>Crypto Playground</h2>
-        <h6 style="color:grey; font-style: italic;">A simple tool to learn basic concepts of cryptography!</h6>
-        <hr>
+        <h6
+          style="color:grey; font-style: italic;"
+        >A simple tool to learn basic concepts of cryptography!</h6>
+        <hr style="opacity: 1.5" />
       </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -45,8 +46,8 @@
 }
 
 .centeralign {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
--- src/views/Crypto.vue
+++ src/views/Crypto.vue
@@ -1,152 +1,65 @@
 <style scoped>
 .addmargin {
-    margin-top: 10px;
-    margin-bottom: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
 
 .vue-logo-back {
-    background-color: black;
+  background-color: black;
 }
 
-.logo{
-	width: 144px;
+.logo {
+  width: 144px;
 }
 
-.fullbody{
+.fullbody {
   width: 100%;
 }
 </style>
 <template>
-<div class="home">
+  <div class="home">
     <div class="col-md-8 centeralign">
-      <mdb-container>
-          <mdb-row class="mt-12">
-            <mdb-card class="text-center fullbody">
-              <mdb-card-header>
-                <mdb-tab default class="card-header-tabs">
-                  <mdb-tab-item :active="active==0" @click.native.prevent="active=0">Introduction</mdb-tab-item>
-                  <mdb-tab-item :active="active==1" @click.native.prevent="active=1">Hashing</mdb-tab-item>
-                  <mdb-tab-item :active="active==2" @click.native.prevent="active=2">Symmetric</mdb-tab-item>
-                  <mdb-tab-item :active="active==3" @click.native.prevent="active=3">Asymmetric</mdb-tab-item>
-                </mdb-tab>
-              </mdb-card-header>
-              <mdb-card-body style="min-height: 484px">
-                <mdb-tab-content>
-                  <mdb-tab-pane class="fade" key="show0" v-if="active==0">
-                    <div>
-                      This is introduction
-                    </div>
-                  </mdb-tab-pane>
-                  <mdb-tab-pane class="fade" key="show1" v-if="active==1">
-                    <Hash/>
-                  </mdb-tab-pane>
-                  <mdb-tab-pane class="fade" key="show2" v-if="active==2">
-                    <Symmetric/>
-                  </mdb-tab-pane>
-                  <mdb-tab-pane class="fade" key="show3" v-if="active==3">
-                    <Asymmetric/>
-                  </mdb-tab-pane>
-                </mdb-tab-content>
-              </mdb-card-body>
-            </mdb-card>
-          </mdb-row>
-        </mdb-container>
-
-        <!-- <div class="card centeralign addmargin" style="width: 18rem;" v-for="customer in customerlist" :key="customer.id">
-            <div class="card-body" v-on:click="setSelectedCustomer(customer.name)">
-                <h5 class="card-title">{{customer.name}}</h5>
-                <p class="card-text">{{customer.email}}</p>
-                <p class="card-text">{{customer.phone}}</p>
-
-                <a class="btn btn-primary" v-on:click="goToDetailsPage(customer.id)"><span style="color:white">Click for more details</span></a>
-            </div>
-        </div> -->
+      <b-card no-body style="padding: 20px">
+        <b-tabs content-class="mt-3">
+          <b-tab title="Introduction" active>
+            <p>This is introduction</p>
+          </b-tab>
+          <b-tab title="Hashing">
+            <Hash />
+          </b-tab>
+          <b-tab title="Symmetric">
+            <Symmetric />
+          </b-tab>
+          <b-tab title="Asymmetric">
+            <Asymmetric />
+          </b-tab>
+        </b-tabs>
+      </b-card>
     </div>
-    <!-- <Display v-if="selectedCustomer!=''" :selectedCustomer="selectedCustomer" /> -->
-</div>
-
+  </div>
 </template>
 
 <script>
-
-import Hash from '@/components/Hash.vue'
-import Asymmetric from '@/components/Asymmetric.vue'
-import Symmetric from '@/components/Symmetric.vue'
-// import axios from 'axios'
-import { mdbContainer,  mdbRow, mdbCol, mdbBtn, mdbCard, mdbCardTitle, mdbCardText, mdbCardFooter, mdbCardBody, mdbCardHeader, mdbListGroup, mdbListGroupItem, mdbNavItem, mdbCardGroup, mdbIcon, mdbFooter, mdbTab, mdbTabItem, mdbTabContent, mdbTabPane } from 'mdbvue';
-
-  export default {
-    name: 'PanelPage',
-    components: {
-      mdbContainer,
-      mdbRow,
-      mdbCol,
-      mdbBtn,
-      mdbCard,
-      mdbCardTitle,
-      mdbCardText,
-      mdbCardFooter,
-      mdbCardBody,
-      mdbCardHeader,
-      mdbListGroup,
-      mdbListGroupItem,
-      mdbNavItem,
-      mdbCardGroup,
-      mdbIcon,
-      mdbFooter,
-      mdbTab,
-      mdbTabItem,
-      mdbTabContent,
-      mdbTabPane,
-      Hash,
-      Asymmetric,
-      Symmetric
-    },
-    data(){
-      return {
-        active: 0
-      };
-    },
-    created(){
-
-    },
-    methods: {
-      gotosubpage: (id) => {
-          this.$router.push(`${id}`);
-      }
+import Hash from "@/components/Hash.vue";
+import Asymmetric from "@/components/Asymmetric.vue";
+import Symmetric from "@/components/Symmetric.vue";
+export default {
+  name: "PanelPage",
+  components: {
+    Hash,
+    Asymmetric,
+    Symmetric
+  },
+  data() {
+    return {
+      active: 0
+    };
+  },
+  created() {},
+  methods: {
+    gotosubpage: id => {
+      this.$router.push(`${id}`);
     }
   }
-
-// export default {
-//     name: 'customers',
-//     mounted() {
-//         axios({
-//             method: "GET",
-//             "url": "assets/samplejson/customerlist.json"
-//         }).then(response => {
-//             this.customerlist = response.data;
-//         }, error => {
-//             // eslint-disable-next-line
-//             console.error(error);
-//         });
-//     },
-//     data() {
-//         return {
-//             customerlist: [],
-//             selectedCustomer: ""
-//         }
-//     },
-//     components: {
-//         Display
-//     },
-//     methods: {
-//         setSelectedCustomer: function(name) {
-//             this.selectedCustomer = name;
-//         },
-//         goToDetailsPage: function(id) {
-//             this.$router.push("/customerdetails/"+id);
-//         }
-//     }
-// }
-
+};
 </script>
