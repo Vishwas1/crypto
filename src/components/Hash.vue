658195e14bab3c974f8ba1d99203d2055a27c340
vishwas1 <vishwasanand.cse@gmail.com>
2020-06-15 12:44:44 +0530

    Added HASH functionality

--- src/components/Hash.vue
+++ src/components/Hash.vue
@@ -1,6 +1,45 @@
+<style scoped>
+.floatLeft{
+    float: left;
+}
+</style>
+
 <template>
 <div>
-    <p>Hash component</p>
-    <p>With supporting text below as a natural lead-in to additional content.</p>
+    <form action="#" class="col-md-8">
+        <div class="form-group">
+            <label class="floatLeft">Data:</label>
+            <textarea  class="form-control" @change="gotosubpage()" v-model="plaintext" rows="8" cols="50"></textarea>
+        </div>
+        <div class="form-group">
+            <label class="floatLeft">Hash:</label>
+            <input type="text" class="form-control" v-model="hashtext"  disabled>
+        </div>
+    </form>
 </div>
 </template>
+
+
+<script>
+
+import symmetric from '../crypto-lib/symmetric'
+
+export default {
+    name: 'HashPage',
+    data(){
+      return {
+        plaintext: "",
+      };
+    },
+    methods: {
+      calculateHash: function(){
+          return symmetric.sha256hashStr(this.plaintext)
+      }
+    },
+    computed:{
+        hashtext: function(){
+            return this.calculateHash()
+        },
+    },
+  }
+</script>
