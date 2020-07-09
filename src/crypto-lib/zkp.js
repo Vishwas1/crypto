b0d5948009056cf676f96754d0fb79a053a5dfb9
vishwas1 <vishwasanand.cse@gmail.com>
2020-07-09 13:26:42 +0530

    bug fix for ActualAge < AgeToProve

--- src/crypto-lib/zkp.js
+++ src/crypto-lib/zkp.js
@@ -15,7 +15,7 @@ const generateProof = (actualAge, ageToProof) => {
     if(!actualAge || !ageToProof) throw new Error(`Invalid actual age = ${actualAge} or age to proof = ${ageToProof}`)
     actualAge = parseInt(actualAge);
     ageToProof = parseInt(ageToProof);
-    const randomString = asymm.generateKeyPair().publicKey;
+    const randomString = "random_" + asymm.generateKeyPair().publicKey;
     return {
         proof: getIteratedHash(1+ actualAge, randomString),
         challange: getIteratedHash(1+ actualAge - ageToProof, randomString),
@@ -25,6 +25,7 @@ const generateProof = (actualAge, ageToProof) => {
 const verifyProof = (ageToProof, provingKit) => {
     const { challange, proof } = provingKit;
     if(!challange || !proof) throw new Error('Invalid proving kit');
+    if(challange.indexOf('random_') > 0) return false
     const temp_proof = getIteratedHash(ageToProof, challange)
     if(temp_proof === proof) return true
     return false
