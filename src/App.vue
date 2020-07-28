2f0d211d392f471a1728c550f394a321903e8f6f
Vishwas Anand Bhushan <vishwasanand.cse@gmail.com>
2020-07-28 09:35:35 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,10 @@
     <div class="row">
       <div class="col-md-8 centeralign logo leftAlign">
         <!-- <h3><img class="logo" src="https://forum.aeternity.com/uploads/db0917/original/1X/543f494a818af66690a1298689af3fdd0c6389a1.png"> | Laboratory</h3> -->
-        <h2>Crypto Playground</h2>
+        <h2>Cryptography Playground</h2>
         <h6
           style="color:grey; font-style: italic;"
-        >A simple tool to learn basic concepts of cryptography!</h6>
+        >A simple tool to play and learn basic concepts of cryptography!</h6>
         <hr style="opacity: 1.5" />
       </div>
     </div>
